3c9e1f6d3cba8635fdd5ef4ed0aa345b26282ea8
Ishwarya <ishusankar14@gmail.com>
2025-07-31 16:56:47 +0530

    event commit
    commit for all the DOM and event

---
+++ notes/datatype.js
@@ -0,0 +1,30 @@
+
+//string
+let name = "Ishwarya"
+//number 
+let num = 124
+//boolean
+let isTester = true
+//undefined 
+let sample
+console.log(sample);
+//discount
+let discount = null;
+//bigint
+ let bigNum = 1234567890123456789012345678901234567890n;
+ 
+ //NON-PRIMITIVE DATA TYPES
+ //Objects
+ let user = {
+  name: "Ishwarya",
+  role: "QA Engineer",
+  experience: 3
+};
+//function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+//
+
+
+
